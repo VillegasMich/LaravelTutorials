b53aed3387222ef1d1740362b5c16d4685df1744
VillegasMich <manuel.villegas.michel@gmail.com>
2025-02-25 13:08:39 -0500

    Tutorial03-A Completed

---
+++ Tutorial03/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Home Routes
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/about', function () {
+    $data1 = 'About us - Online Store'; // title
+    $data2 = 'About us'; // subtitle
+    $description = 'This is the about page';
+    $author = 'Developed by: Mauel Villegas';
+
+    return view('home.about')->with('title', $data1)->with('subtitle', $data2)->with('description', $description)->with('author', $author);
+})->name('home.about');
+Route::get('/contact', function () {
+    $data1 = 'Contact us - Online Store'; // title
+    $data2 = 'Contact us'; // subtitle
+    $description = 'This is the contact page';
+
+    return view('home.contact')->with('title', $data1)->with('subtitle', $data2)->with('description', $description);
+})->name('home.contact');
+
+// Product Routes
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+// Cart Routes
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
